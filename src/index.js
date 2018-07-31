4ee3f433b2789bae4b381dd64861009cff431e60
Matti Aho <ookooko@gmail.com>
2018-07-31 13:38:12 +0300

    card mappi

--- src/index.js
+++ src/index.js
@@ -7,10 +7,15 @@ import 'tachyons';
 import {robots} from './robots'
 
 ReactDOM.render(
+  // <div>
+  //   <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+  //   <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+  //   <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+  // </div>
   <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+  {robots.map((robot) => 
+    <Card id={robot.id} name={robot.name} email={robot.email}/>    
+  )}
   </div>
   ,document.getElementById('root'));
 registerServiceWorker();
